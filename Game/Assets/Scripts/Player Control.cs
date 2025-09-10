77186b50eec503885b82a5ce7d8e18839c2df61d
Wilson-Productions <t.richerson@icloud.com>
2025-09-10 13:47:07 -0700

    Added Movement

---
+++ Game/Assets/Scripts/Player Control.cs
@@ -0,0 +1,36 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+   Rigidbody2D rb;
+
+    float inputX;
+    float inputY;
+
+    public float speed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 tempMove = rb.linearVelocity;
+
+            tempMove.x = inputX * speed;
+
+        rb.linearVelocity = (tempMove.x * transform.right) +
+                            (tempMove.y * transform.up);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 InputAxis = context.ReadValue<Vector2>();
+
+        inputX = InputAxis.x;
+    }
+}
