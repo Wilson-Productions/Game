c1f10660ed114a5b048d9cf837c6b7fcccc6ceed
Wilson-Productions <t.richerson@icloud.com>
2025-09-17 13:37:58 -0700

    Jump, Enemy, HP

--- Game/Assets/Scripts/Player Control.cs
+++ Game/Assets/Scripts/Player Control.cs
@@ -1,18 +1,24 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
-   Rigidbody2D rb;
+    Rigidbody2D rb;
+    Transform respawnPoint;
+
+    public int lives = 3;
+    public int health = 3;
+    public int maxHealth = 5;
 
     float inputX;
-    float inputY;
-
+    public float jumpHeight = 5;
+    public float groundDetectDistance = 1.1f;
     public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        respawnPoint = GameObject.Find("RespawnPoint").transform;
         rb=GetComponent<Rigidbody2D>();
 
         Cursor.visible = false;
@@ -22,6 +28,19 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (health <= 0)
+        {
+            if(lives > 0)
+            {
+                transform.position = respawnPoint.position;
+                health = maxHealth;
+                lives--;
+            }
+
+            if(lives <= 0)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
         // Movement System
         Vector2 tempMove = rb.linearVelocity;
 
@@ -36,5 +55,27 @@ public class PlayerControl : MonoBehaviour
         Vector2 InputAxis = context.ReadValue<Vector2>();
 
         inputX = InputAxis.x;
+    }
+
+    public void Jump()
+    {
+        if (Physics2D.Raycast(transform.position, -transform.up, groundDetectDistance))
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "basicenemy")
+            health--;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "health" && health < maxHealth)
+        {
+            health++;
+            Destroy(collision.gameObject);
+        }
     }
 }
