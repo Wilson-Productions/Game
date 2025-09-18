a28ae02b98bd3a9bbe7799c9d407a7066b191c23
Wilson-Productions <t.richerson@icloud.com>
2025-09-18 13:48:53 -0700

    Enemy Movement

--- Game/Assets/Scripts/Enemy AI.cs
+++ Game/Assets/Scripts/Enemy AI.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    Rigidbody2D enemyRB;
+
+    public float edgeCheckDistance = 1.0f;
+    public float speed = 2.5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        enemyRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 tempSpeed = enemyRB.linearVelocity;
+
+        if (!Physics2D.Raycast(transform.position, -transform.up, edgeCheckDistance))
+        {
+            speed *= -1;
+        }
+
+        tempSpeed.x = speed;
+
+        enemyRB.linearVelocity = tempSpeed;
     }
 }
--- Game/Assets/Scripts/Player Control.cs
+++ Game/Assets/Scripts/Player Control.cs
@@ -63,7 +63,7 @@ public class PlayerControl : MonoBehaviour
             rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
 
     }
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "basicenemy")
