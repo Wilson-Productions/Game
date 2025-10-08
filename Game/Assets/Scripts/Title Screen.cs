33e33ec853374416ea343fa9c6831e284c8d1c61
Wilson-Productions <t.richerson@icloud.com>
2025-10-08 12:23:42 -0700

    Whole buncha crap

---
+++ Game/Assets/Scripts/Title Screen.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleScreen : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
