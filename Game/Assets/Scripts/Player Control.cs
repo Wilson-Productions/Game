3ee1d06f9cebcbb6e3f51dd6efbeabd4682decac
Wilson-Productions <t.richerson@icloud.com>
2025-10-17 13:45:26 -0700

    more anims

--- Game/Assets/Scripts/Player Control.cs
+++ Game/Assets/Scripts/Player Control.cs
@@ -1,7 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
